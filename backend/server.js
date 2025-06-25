c97e73ab942a30e98800da197ede879cbad4d6c8
Utkarsh Pratap Singh <113666652+Thakurutkarsh07@users.noreply.github.com>
2025-06-25 12:19:07 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://expense-tracker-1-70ug.onrender.com",
   credentials: true
 }));
 
